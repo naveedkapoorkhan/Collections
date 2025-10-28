b0aa4b8d4813cbf3aa4bcc2b338e34bc067b304d
naveedkapoorkhan <naveedkapoorkhan@gmail.com>
2025-10-28 13:26:56 +0500

    Linq queries Writing, IQuerble, Ienumerator

--- Collections/Program.cs
+++ Collections/Program.cs
@@ -1,97 +1,88 @@
 ﻿using System;
 using System.Collections.Generic;
-public class Student
-{
-    public string Name { get; set; }
-    public int Age { get; set; }
-
-    public override bool Equals(object? obj)
-    {
-        if (obj == null || GetType() != obj.GetType())
-            return false;
-
-        Person other = (Person)obj;
-        return Name == other.Name && Age == other.Age;
-    }
-
-    public override int GetHashCode()
-    {
-        return (Name, Age).GetHashCode();
-    }
-}
+using System.Linq;
 
 public class Person
 {
     public string Name { get; set; }
     public int Age { get; set; }
-
-    public override bool Equals(object? obj)
-    {
-        if (obj == null || GetType() != obj.GetType())
-            return false;
-
-        Person other = (Person)obj;
-        return Name == other.Name && Age == other.Age;
-    }
-
-    public override int GetHashCode()
-    {
-        return (Name, Age).GetHashCode();
-    }
 }
 
 public class Program
-{  
-   
+{
     public static void Main()
     {
-       
-        Person p1 = new Person { Name = "Naveed", Age = 22 };
-        Person p2 = new Person { Name = "Naveed", Age = 22 };
-        Person p3 = new Person { Name = "Ali", Age = 25 };
-        Student s = new Student { Name = "Ali", Age = 25 };
-
-
-        List<Person> people= new List<Person> { p1,p2,p3 };
-        List<Person> people1 = new List<Person> { p1, p2 };
-        List<Student> student = new List<Student> { s };
-
-        var unionok = people.Union(people1);
-        foreach (var n in unionok) {
-
-            Console.WriteLine($"{n.Name},{n.Age}");
-        }
-        Console.Write("...........................");
-
-        var intersection = people.Intersect(people1);
-        foreach (var n in intersection)
+        List<Person> people = new List<Person>
         {
+            new Person { Name = "Naveed", Age = 22 },
+            new Person { Name = "kabeer", Age = 24 },
+            new Person { Name = "ishraq", Age = 243 },
+            new Person { Name = "mubeen", Age = 232 },
+            new Person { Name = "NDK", Age = 212 },
+            new Person { Name = "Naveed", Age = 222 },
+            new Person { Name = "kapoor", Age = 322 }
+        };
 
-            Console.WriteLine($"{n.Name},{n.Age}");
+        Console.WriteLine("..........Method Syntax..........");
+
+        var query1 = people.Where(s => s.Age > 100).Select(s => s.Name);
+        var query2 = people.Where(s => s.Age > 100).Select(s => new { s.Name, s.Age });
+
+        Console.WriteLine("Query1:");
+        foreach (var x in query1)
+        {
+            Console.WriteLine($"Name: {x}");
         }
 
-        
+        Console.WriteLine("Query2:");
+        foreach (var x in query2)
+        {
+            Console.WriteLine($"Name: {x.Name}, Age: {x.Age}");
+        }
 
-        Person p4 = p1;
-        Console.WriteLine($"p3.Equals(s): {p3.Equals(s)}");
-        Console.WriteLine($"p1.Equals(p4): {p1.Equals(p4)}");
-        Console.WriteLine($"people.Contains(p4): {people.Contains(p4)}");
-        Console.WriteLine($"p1.Equals(p2): {p1.Equals(p2)}");
-        Console.WriteLine($"p1.Equals(p3): {p1.Equals(p3)}");
-        Console.WriteLine($"people.Contains(p2): {people.Contains(p2)}");
-        Console.WriteLine($"people.Contains(p3): {people.Contains(p3)}");
+        Console.WriteLine();
+        Console.WriteLine("..........Query Syntax..........");
 
+        var query3 = from s in people
+                     where s.Age == 22
+                     select s.Name;
 
+        Console.WriteLine("Query3:");
+        foreach (var x in query3)
+        {
+            Console.WriteLine($"Name: {x}");
+        }
 
-        Console.WriteLine("........."); 
-        Console.WriteLine("---- Hash Codes ----");
-        Console.WriteLine($"p1 HashCode: {p1.GetHashCode()}");
-        Console.WriteLine($"p2 HashCode: {p2.GetHashCode()}");
-        Console.WriteLine($"p3 HashCode: {p3.GetHashCode()}");
-        Console.WriteLine($"p4 HashCode: {p4.GetHashCode()}");
+        var query4 = from c in people
+                     where c.Name == "ishraq"
+                     select new { c.Name, c.Age };
 
+        Console.WriteLine("Query4:");
+        foreach (var x in query4)
+        {
+            Console.WriteLine($"Name: {x.Name}, Age: {x.Age}");
+        }
 
+        Console.WriteLine();
+        Console.WriteLine("..........IQueryable and IEnumerable..........");
 
-        
+        IQueryable<Person> methodSyntax = people.AsQueryable()
+                                                .Where(std => std.Name == "ishraq");
+
+        Console.WriteLine("IQueryable:");
+        foreach (var student in methodSyntax)
+        {
+            Console.WriteLine($"Name is: {student.Name}");
+        }
+
+        IEnumerable<string> methodSyntax1 = people
+                                            .Where(std => std.Name == "ishraq")
+                                            .Select(std => std.Name);
+
+        Console.WriteLine("IEnumerable<string>:");
+        foreach (var name in methodSyntax1)
+        {
+            Console.WriteLine($"Name is: {name}");
+        }
     }
 }
